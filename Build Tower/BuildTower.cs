221399f90f9c0f9792836c5284c179a038e7021d
Egor Medv <egormed043@gmail.com>
2022-07-27 11:13:56 +0300

    Добавил: Build Tower Mexican Waves Two Sum Your order, please

---
+++ Build Tower/BuildTower.cs
@@ -0,0 +1,60 @@
+﻿/*
+Build Tower
+Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+*/
+
+// Solution 1
+public class Kata
+{
+    public static string[] TowerBuilder(int nFloors)
+    {
+        string[] result = new string[nFloors];
+        int length = (nFloors - 1) * 2 + 1;
+
+        for (int i = 0; i < nFloors; i++)
+        {
+            int star = i * 2 + 1;
+            int gap = (length - star) / 2;
+            string str = "";
+            for (int a = 0; a < gap; a++) { str += " "; }
+            for (int a = 0; a < star; a++) { str += "*"; }
+            for (int a = 0; a < gap; a++) { str += " "; }
+            result[i] = str;
+        }
+
+        return result;
+    }
+}
+
+
+//Solution 2
+public class Kata
+{
+    public static string[] TowerBuilder(int nFloors)
+    {
+        var result = new string[nFloors];
+        for (int i = 0; i < nFloors; i++)
+            result[i] = string.Concat(new string(' ', nFloors - i - 1),
+                                      new string('*', i * 2 + 1),
+                                      new string(' ', nFloors - i - 1));
+        return result;
+    }
+}
---
+++ Your order, please/YourOrder.cs
@@ -0,0 +1,51 @@
+﻿/*
+Your task is to sort a given string. Each word in the string will contain a single number. 
+This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only 
+contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+
+// Solution 1
+public static class Kata
+{
+    public static string Order(string words)
+    {
+        if (string.IsNullOrEmpty(words)) return string.Empty;
+
+        string[] wordsArray = words.Split();
+        string[] result = new string[wordsArray.Length];
+
+        foreach (string word in wordsArray)
+        {
+            foreach (char ch in word)
+            {
+                if (char.IsDigit(ch))
+                {
+                    int preIndex = ch - '0';
+                    result[preIndex - 1] = word;
+                }
+            }
+        }
+        return string.Join(" ", result);
+    }
+}
+
+
+// Solution 2
+public static class Kata
+{
+    public static string Order(string words)
+    {
+        if (string.IsNullOrEmpty(words)) return words;
+
+        return string.Join(" ", words.Split(' ').OrderBy(s => s.ToList().Find(c => char.IsDigit(c))));
+    }
+}
